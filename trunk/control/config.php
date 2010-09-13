ebadaea16bd823b0c601f0400654a93f4055404c
rudmz.flk <rudmz.flk@f88962d4-6c5c-5e9b-a679-cb9c3e0fc918>
2010-09-13 12:03:02 +0000

    [+] перенос блоков в php-модули

---
+++ trunk/control/config.php
@@ -0,0 +1,22 @@
+<?php
+	unset($content_tree);
+
+	// главная страница (по умолчанию)
+	$content_tree['']['']['']['caption'] = 'Новичкам';
+	$content_tree['']['']['']['caption'] = 'newbee_about.html';
+
+	$content_tree['newbee']['']['']['caption'] = 'Новичкам';
+	$content_tree['newbee']['']['']['caption'] = 'newbee_about.html';
+
+	$content_tree['newbee']['about']['']['caption'] = 'Что это такое?';
+	$content_tree['newbee']['about']['']['content'] = 'newbee_about.html';
+
+	$content_tree['newbee']['about']['game']['caption'] = 'Об игре';
+	$content_tree['newbee']['about']['game']['content'] = 'newbee_about_game.html';
+
+	$content_tree['newbee']['download']['']['caption'] = 'Как скачать и установить';
+	$content_tree['newbee']['download']['']['content'] = 'newbee_download.html';
+
+	$content_tree['newbee']['takeoff']['']['caption'] = 'Как взлететь';
+	$content_tree['newbee']['takeoff']['']['content'] = 'newbee_takeoff.html';
+?>
---
+++ trunk/control/menu.php
@@ -0,0 +1,45 @@
+<?php
+	echo '<ul>
+				<li class="current">
+					<h2><a href="#">Что это такое?</a></h2>
+					<ul>
+						<li><a href="index.php?category=newbee&section=about&part=game">Об игре</a></li>
+						<li><a href="#">О сервере</a></li>
+						<li><a href="#">Разработчики</a></li>
+						<li><a href="#">Сообщество</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2><a href="#">Как скачать и установить</a></h2>
+					<ul style="display:none;">
+						<li><a href="#">Скачать</a></li>
+						<li><a href="#">Установка</a></li>
+						<li><a href="#">Решение вопросов</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2><a href="#">Как взлететь</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">Управление</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">Основные манёвры</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">Основы тактики</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">Самолёты</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">Форум</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">Эскадрильи</a></h2>
+				</li>
+				<li>
+					<h2><a href="#">FAQ</a></h2>
+				</li>
+			</ul>';
+?>
